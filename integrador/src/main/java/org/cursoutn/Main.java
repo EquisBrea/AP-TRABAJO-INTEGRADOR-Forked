46a6e906715bbb120809aacefcff352038290114
EquisBrea <142422819+EquisBrea@users.noreply.github.com>
2023-11-24 19:23:22 -0300

    Armado de clases básicas, y update DER
    Más correción en el modelado, algunos atributos más, mejores relaciones (falta emprolijar)

--- integrador/src/main/java/org/cursoutn/Main.java
+++ integrador/src/main/java/org/cursoutn/Main.java
@@ -2,6 +2,10 @@ package org.cursoutn;
 
 public class Main {
     public static void main(String[] args) {
+        saludarAlMundo();
+    }
+
+    private static void saludarAlMundo () {
         System.out.println("Hello world!");
     }
 }
